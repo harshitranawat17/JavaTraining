8ef8ea7d9cb03162af4c7592a7a71f4ccd8b5438
harshit ranawat <harshit.ranawat@brainvire.com>
2024-01-08 12:28:43 +0530

    SpringBoot

---
+++ SpringBoot/Microservices/RatingService/src/main/java/com/lcwd/rating/services/impl/RatingServiceImpl.java
@@ -0,0 +1,42 @@
+package com.lcwd.rating.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lcwd.rating.entities.Rating;
+import com.lcwd.rating.repository.RatingRepository;
+import com.lcwd.rating.services.RatingService;
+
+@Service
+public class RatingServiceImpl implements RatingService{
+
+	@Autowired
+	private RatingRepository ratingRepository;
+	
+	@Override
+	public Rating create(Rating rating) {
+		
+		return ratingRepository.save(rating);
+	}
+
+	@Override
+	public List<Rating> getRatings() {
+		
+		return ratingRepository.findAll();
+	}
+
+	@Override
+	public List<Rating> getRatinByUserId(String userId) {
+		
+		return ratingRepository.findByUserId(userId);
+	}
+
+	@Override
+	public List<Rating> getRatingByHotelId(String hotelId) {
+		
+		return ratingRepository.findByHotelId(hotelId);
+	}
+
+}
---
+++ SpringBoot/Spring-security-3.0.Token/src/main/java/com/springboot/security/dto/AuthReq.java
@@ -0,0 +1,23 @@
+package com.springboot.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthReq {
+	private String username;
+	private String password;
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
